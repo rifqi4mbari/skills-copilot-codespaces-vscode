e89cba3c66dbf46a21e1999198d44e098dfe1170
Rifqi Ambari <93808557+rifqi4mbari@users.noreply.github.com>
2025-02-16 09:50:46 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,42 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Use body parser to parse JSON data
+app.use(bodyParser.json());
+
+// Crate comments array
+const comments = [
+  {
+    username: 'Alice',
+    comment: 'I love it!'
+  },
+  {
+    username: 'Bob',
+    comment: 'Good stuff'
+  }
+];
+
+// Get all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// Add a new comment
+app.post('/comments', (req, res) => {
+  const newComment = {
+    username: req.body.username,
+    comment: req.body.comment
+  };
+  comments.push(newComment);
+  res.json(newComment);
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+// Run this file with Node.js and test it with Postman
+// 1. Start
